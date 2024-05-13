8842df1705c1833d625cc9e2cc16ed51e11c83ac
dairen <djean032@gmail.com>
2024-05-13 14:08:04 -0400

    3

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -32,7 +32,8 @@ return { -- Autoformat
             -- You can use a sub-list to tell conform to run *until* a formatter
             -- is found.
             javascript = { "prettier" },
-            svelte = { "prettier" }
+            svelte = { "prettier" },
+            html = { "prettier" },
         },
     },
 }
